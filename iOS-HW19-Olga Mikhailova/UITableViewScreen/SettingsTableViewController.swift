4891f24e9c6bc235b5a77109a5cc8b8c519e7f2b
Olga Mikhailova <foxx_fire@mail.ru>
2025-07-28 17:57:06 +0300

    Configure App and Scene delegates

---
+++ iOS-HW19-Olga Mikhailova/UITableViewScreen/SettingsTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsViewController.swift
+//  iOS-HW19-Olga Mikhailova
+//
+//  Created by FoxxFire on 28.07.2025.
+//
+
+import UIKit
+
+final class SettingsViewController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- iOS-HW19-Olga Mikhailova/UITableViewScreen/SettingsViewController.swift
+++
@@ -1,7 +0,0 @@
-//
-//  SettingsViewController.swift
-//  iOS-HW19-Olga Mikhailova
-//
-//  Created by FoxxFire on 28.07.2025.
-//
-
